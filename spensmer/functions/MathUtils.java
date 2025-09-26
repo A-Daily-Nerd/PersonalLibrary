8851f12883f5275dda818eb333264dc15b00aec8
A-Daily-Nerd <adailynerd@gmail.com>
2025-09-26 05:00:57 -0700

    Added Functionality to Array2d to find neighbors of a point<Integer> a

--- spensmer/functions/MathUtils.java
+++ spensmer/functions/MathUtils.java
@@ -26,6 +26,11 @@ public class MathUtils
         return finalValue;
     }
 
+    /**
+     * @param a First array to add together
+     * @param b Second array to add together
+     * @return a new Array2d<Double> that is the sum of Arrays a and b
+     */
     public static <T extends Number, J extends Number> Array2d<Double> matrixAddition(Array2d<T> a, Array2d<J> b) {
         if ((a.width != b.width) || (a.height != b.height)) {
             throw new ArithmeticException("Matricies have to be the same size");
@@ -39,5 +44,21 @@ public class MathUtils
             finalArray2d.setValue(point, sum); // store the new value back into a
         });
         return finalArray2d;
+    }
+
+
+    public static Point<Integer>[] findNeighbors(Point<Integer> inital) {
+        if (((a.x == 0) || (a.y == 0)) || ((a.x == width) || (a.y == height))) { //Edge cases #IMPLIMENT LATER
+        }
+        Point<Integer>[] returnValues = new Point<Integer>[8];
+        returnValues[0] = new Point<Integer>(inital.x-1,inital.y+1);
+        returnValues[1] = new Point<Integer>(inital.x,inital.y+1);
+        returnValues[2] = new Point<Integer>(inital.x+1,inital.y+1);
+        returnValues[3] = new Point<Integer>(inital.x-1,inital.y);
+        returnValues[4] = new Point<Integer>(inital.x+1,inital.y);
+        returnValues[5] = new Point<Integer>(inital.x-1,inital.y-1);
+        returnValues[6] = new Point<Integer>(inital.x,inital.y-1);
+        returnValues[7] = new Point<Integer>(inital.x+1,inital.y-1);
+        return returnValues;
     }
 }
