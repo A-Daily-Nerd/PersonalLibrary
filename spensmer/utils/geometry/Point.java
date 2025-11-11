64542dd4b83fbdd30d1f306986d224a25d7d23f3
A-Daily-Nerd <adailynerd@gmail.com>
2025-11-11 11:17:11 -0500

    Update Point.java
    Added proper encapsulation to Point.java and getter and setter methods

--- spensmer/utils/geometry/Point.java
+++ spensmer/utils/geometry/Point.java
@@ -2,16 +2,23 @@ package spensmer.utils.geometry;
 
 public class Point<type extends Number>
 {
-    public type x;
-    public type y;
+    private type x;
+    private type y;
     // Constructor
     public Point(type X, type Y) {
         x = X;
         y = Y;
     }
 
+    // Getter/Setter methods for Point
+    public type getX() {return x;}
+    public type getY() {return y;}
+    public void setX(type x) {this.x = x;}
+    public void setY(type y) {this.y = y;}
+
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")"; 
     }
+
 }
